2b2c065ae6fd49441d6ee88e6decd9c5668d2309
remi6901 <descollonges.remi@gmail.com>
2022-06-01 14:15:29 +0200

    change the color of link

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,36 +1,54 @@
 console.log("connecté");
+
+/*==== Change avatar ====*/
 const avatar = document.querySelector(".avatar");
 avatar.addEventListener("click", function(){
     avatar.src = "image/avatar.svg"
 });
 
+
+/**==== Change the name and color ====/ */
 const myName = document.querySelector("#name");
 const button = document.querySelector(".button")
-const background = document.querySelector(".pink-bg")
+const pinkBg = document.querySelectorAll(".pink-bg");
+const pinkText = document.querySelectorAll(".pink-text")
+
 button.addEventListener("click", function(){
     const userName = prompt("What's your name?");
+    const color = prompt("Choose your color.")
     myName.innerHTML = userName;
     myName.style.color = "white";
-    background.style.backgroundColor = "#750ff7"
-    button.style.backgroundColor = "#750ff7"
+    for(let i = 0; i < pinkBg.length; i++){
+        pinkBg[i].style.backgroundColor = color
+    }
+    button.style.backgroundColor = color
+    buttonTools.style.backgroundColor = color
+    for(let j = 0; j < pinkText.length; j++){
+        pinkText[j].style.color = color
+    }   
 })
 
-/*
-const link = document.querySelectorAll(".link");
-for(let i = 0; i < link.length; i++){
-    link[i].addEventListener("mouseover", function(){
-        link.style.color = "#750ff7"
-    })
-}*/
 
+
+/*==== Change the value for li ====*/ 
 const devTools = document.querySelectorAll("#front-dev-tools li");
 const buttonTools = document.querySelector(".button-tools");
 const myTools = ["VSCode", "Github", "Terminal"];
 buttonTools.addEventListener("click", function(){
     for(let i = 0; i < devTools.length; i++){
-        devTools[i].innerHTML = "toto"
+        devTools[i].innerHTML = myTools[i]
     }
 })
 
 
+/*==== change color of link ====*/
+const link = document.querySelectorAll(".link");
+for(let i in link){
+    link[i].addEventListener("mouseover", function(){
+        link[i].style.color = "#750ff7"
+    })
+    link[i].addEventListener("mouseleave", function(){
+        link[i].style.color = "black"
+    })
+}
 
