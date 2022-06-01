56f55816588616cb034e5eec8edaa5d741a8fdf8
remi6901 <descollonges.remi@gmail.com>
2022-06-01 11:09:39 +0200

    change the name by userName and change the color of name

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,10 @@
-console.log("connecté")
-const avatar = document.querySelector(".avatar")
+console.log("connecté");
+const avatar = document.querySelector(".avatar");
 avatar.addEventListener("click", function(){
     avatar.src = "image/avatar.svg"
 });
 
+const myName = document.querySelector("#name");
+const userName = prompt("What's your name?");
+myName.innerHTML = userName;
+myName.style.color = "white";
